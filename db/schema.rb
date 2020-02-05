44fa5db5ffe0c23677625f52f163d164c3a2f7f8
vesna.vucinic68@gmail.com <vesna@LAPTOP-KHUC3ESJ.localdomain>
2020-02-05 20:36:26 +0000

    add correction in user table

--- db/schema.rb
+++ db/schema.rb
@@ -25,9 +25,9 @@ ActiveRecord::Schema.define(version: 20200205193515) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "pasword_digest"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
