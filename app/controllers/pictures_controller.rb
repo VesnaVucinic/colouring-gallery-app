f7770c5d47111d6bd7b3f2ea4fa151330785e9bc
vesna.vucinic68@gmail.com <vesna@LAPTOP-KHUC3ESJ.localdomain>
2020-02-07 22:06:35 +0000

    add helper method set_picture and patch for edit

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -26,24 +26,47 @@ class PicturesController < ApplicationController
 
   # GET: /pictures/5,show rout will show what already exist
   get "/pictures/:id" do
-    @picture = Picture.find(params[:id])
+    set_picture
     erb :"/pictures/show.html"
   end
 
-  # GET: /pictures/5/edit
+  # GET: /pictures/5/edit, render form edit
   get "/pictures/:id/edit" do
-    @picture = Picture.find(params[:id])
-
-    erb :"/pictures/edit.html"
+    set_picture
+    if logged_in?
+      if @picture.user == current_user
+        erb :"/pictures/edit.html"
+      else
+        redirect "users/#{current_user.id}"
+      end
+    else
+      redirect "/"
+    end
   end
 
-  # PATCH: /pictures/5
+  # PATCH: /pictures/5/modifie-update what is edited 
   patch "/pictures/:id" do
-    redirect "/pictures/:id"
+    set_picture
+    if logged_in?
+      if @picture.user == current_user && params[:title] != "" && params[:description] != "" && params[:image_url]
+        @picture.update(title: params[:title], description: params[:description], image_url: params[:image_url])#this is one big hash , I ommited{}, and it still one argument
+      redirect "/pictures/#{@picture.id}"
+      else
+        redirect "users/#{current_user.id}"
+      end
+    else
+      redirect "/"
+    end
   end
 
   # DELETE: /pictures/5/delete
   delete "/pictures/:id/delete" do
     redirect "/pictures"
+  end
+
+  private #helper method, call private becouse no use outside this class
+
+  def set_picture #when I invoke method return value is value of instance variable @picture
+    @picture = Picture.find(params[:id])
   end
 end
