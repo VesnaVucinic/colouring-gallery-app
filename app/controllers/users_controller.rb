121cd410c8ec0f203eb86414d4b3e8f7655bb73c
vesna.vucinic68@gmail.com <vesna@LAPTOP-KHUC3ESJ.localdomain>
2020-02-07 00:27:43 +0000

    add sessions in post signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,7 @@ class UsersController < ApplicationController
   post "/users" do
    if params[:name] != "" && params[:email] != "" && params[:password] != ""
     @user = User.create(params)
+    session[:user_id] = @user.id
     redirect "/users/#{@user.id}"
    else
     redirect '/signup'
