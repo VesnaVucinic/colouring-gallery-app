eab41484bffb601e25dc1271fe1abc447493f081
vesna.vucinic68@gmail.com <vesna@LAPTOP-KHUC3ESJ.localdomain>
2020-02-08 16:12:05 +0000

    add flash gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,8 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
     enable :sessions
     set :session_secret, "secert_for_development"
+    register Sinatra::Flash
+
   end
 
   get "/" do
